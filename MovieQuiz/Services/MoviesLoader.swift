266e9c039e74e0d7e244c2e1eeb76ddc186dc268
KarinaArtamonova <pakarin@yandex.ru>
2024-09-10 15:25:00 +0300

    Добавлены UI тесты

--- MovieQuiz/Services/MoviesLoader.swift
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -12,12 +12,15 @@ protocol MoviesLoading {
 }
 
 struct MoviesLoader: MoviesLoading {
-    // MARK: - NetworkClient
-    private let networkClient = NetworkClient()
+  // MARK: - NetworkClient
+  private let networkClient: NetworkRouting
+  
+  init(networkClient: NetworkRouting = NetworkClient()) {
+      self.networkClient = networkClient
+  }
     
     // MARK: - URL
     private var mostPopularMoviesUrl: URL {
-        // Если мы не смогли преобразовать строку в URL, то приложение упадёт с ошибкой
         guard let url = URL(string: "https://tv-api.com/en/API/Top250Movies/k_zcuw1ytf") else {
             preconditionFailure("Unable to construct mostPopularMoviesUrl")
         }
@@ -26,17 +29,20 @@ struct MoviesLoader: MoviesLoading {
     
     func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void) {
         networkClient.fetch(url: mostPopularMoviesUrl) { result in
-               switch result {
-               case .success(let data):
-                   do {
-                       let mostPopularMovies = try JSONDecoder().decode(MostPopularMovies.self, from: data)
-                       handler(.success(mostPopularMovies))
-                   } catch {
-                       handler(.failure(error))
-                   }
-               case .failure(let error):
-                   handler(.failure(error))
-               }
-           }
+            switch result {
+            case .success(let data):
+                do {
+                    let mostPopularMovies = try JSONDecoder().decode(MostPopularMovies.self, from: data)
+                    handler(.success(mostPopularMovies))
+                } catch {
+                    handler(.failure(error))
+                }
+            case .failure(let error):
+                handler(.failure(error))
+            }
+        }
     }
 }
+
+
+
